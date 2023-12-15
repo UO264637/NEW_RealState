c11c91d4f5a2c060b655b316d85cab200267bc80
Carmen <uo264637@uniovi.es>
2023-12-15 16:28:36 +0100

    Ampliacion 2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import detectEthereumProvider from "@metamask/detect-provider";
-import { Contract, ethers } from "ethers";
+import { Contract, ethers, utils } from "ethers";
 import myContractManifest from "./contracts/MyContract.json";
 import { useState, useEffect, useRef } from 'react';
+import { decodeError } from 'ethers-decode-error';
 
 function App() {
   const myContract = useRef(null);
@@ -29,27 +30,25 @@ function App() {
   let updateBalances = async () => {
     let balanceFromBlockchain = await myContract.current?.getBalance();
     if (balanceFromBlockchain != null) {
-      let a = ethers.utils.formatEther(balanceFromBlockchain);
-      setBalance(a)
-      console.log(a)
+      let bal = ethers.utils.formatEther(balanceFromBlockchain);
+      setBalance(bal);
     }
     else
-      setBalance(0)
+      setBalance(0);
 
     let balanceWeiFromBlockchain = await myContract.current?.getBalanceWei();
     if (balanceWeiFromBlockchain != null) {
-      let b = balanceWeiFromBlockchain.toString()
-      setBalanceWei(b)
-      console.log(b)
+      let balWei = ethers.utils.formatEther(balanceWeiFromBlockchain);
+      setBalanceWei(balWei);
     }
     else
-      setBalanceWei(0)
+      setBalanceWei(0);
   }
 
   let updateWallet = async () => {
-    let userAddress = await  myContract.current.signer.getAddress();
-        let wb = await  myContract.current.provider.getBalance(userAddress);
-        setWalletBalance(ethers.utils.formatEther(wb));
+    let userAddress = await myContract.current.signer.getAddress();
+    let wb = await myContract.current.provider.getBalance(userAddress);
+    setWalletBalance(ethers.utils.formatEther(wb));
   }
 
   let configureBlockchain = async () => {
@@ -70,28 +69,27 @@ function App() {
         );
         updateWallet();
       }
-    } catch (error) { console.log(error)}
+    } catch (error) { console.log(error) }
   }
 
   let clickBuyTiket = async (i) => {
     if (walletBalance > 0.02) {
-      myContract.current.isTicketAvailable(i).then(
-        (result) => { },
-        (error) => {console.log(error); alert(error.message)}
-      );
-
-      const tx = await myContract.current.buyTiket(i, {
+      const tx = await myContract.current.buyTiket(11, {
         value: ethers.utils.parseEther("0.02"),
         gasLimit: 6721975,
         gasPrice: 20000000000,
-      });
-      await tx.wait();
+      })
 
-      const tiketsUpdated = await myContract.current.getTikets();
-      setTikets(tiketsUpdated);
-
-      updateBalances();
-      updateWallet();
+      try {
+        await tx.wait()
+        const tiketsUpdated = await myContract.current.getTikets();
+        setTikets(tiketsUpdated);
+        updateBalances();
+        updateWallet();
+      } catch (error) {
+        const errorDecoded = decodeError(error);
+        alert('Revert reason: ' + errorDecoded.error);
+      }
     }
     else {
       alert("Not enough BNB")
@@ -100,14 +98,33 @@ function App() {
 
   let withdrawBalance = async () => {
     const tx = await myContract.current.transferBalanceToAdmin().then(
-        (result) => { },
-        (error) => { alert(error.data.message)}
+      (result) => { },
+      (error) => { alert(decodeError(error).error) }
     );
-    await tx.wait();
-    
-    updateBalances();
-    updateWallet();
+    try {
+      await tx.wait();
+
+      updateBalances();
+      updateWallet();
+    }
+    catch (error) { }
   }
+
+  let changeAdmin = async (e) => {
+    //evita que avance a la página del formulario
+    e.preventDefault();
+
+    const inputValue = e.target.elements[0].value;
+    if (utils.isAddress(inputValue)) {
+      await myContract.current.setAdmin(inputValue).then(
+        (result) => { alert("Admin updated")},
+        (error) => { alert(decodeError(error).error) }
+      );
+    }
+    else {
+      alert("Address not valid")
+    }
+  };
 
   return (
     <div>
@@ -117,11 +134,16 @@ function App() {
         <label>Balance Wei: </label> {balanceWei}
         <button onClick={() => withdrawBalance()}>Withdraw Balance</button>
       </p>
+      <form className="form-inline" onSubmit={(e) => changeAdmin(e)}>
+        <label>New Admin: </label>
+        <input type="text" />
+        <button type="submit" > Change </button>
+      </form>
 
       <p>
         <label>Wallet Balance: </label> {walletBalance}<br></br>
       </p>
-      
+
       <ul>
         {tikets.map((address, i) =>
           <li>Tiket {i} comprado por {address}
